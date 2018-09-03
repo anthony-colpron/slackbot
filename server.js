9ffb0dd3a8fab95ad3f3dcf9d6f9fcd42f6ed507
Anton <anthonycolpron@gmail.com>
2018-09-03 17:01:25 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
+app.post('/test', (req, res) => {
+
+
+
+    res.send(req.body.challenge);
+
+})
+
+app.listen(4000);
