e96999d883b4101f21c1d3b94ef8f6e5b05f1483
Anton <anthonycolpron@gmail.com>
2018-09-03 17:42:20 -0400

    app mention

--- server.js
+++ server.js
@@ -7,8 +7,26 @@ app.use(bodyParser.json());
 app.post('/test', (req, res) => {
 
 
+    let payload = req.body;
+    res.sendStatus(200);
 
-    res.send(req.body.challenge);
+    if (payload.event.type === "app_mention") {
+        
+        fetch('https://slack.com/api/chat.postMessage', {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json',
+                'Authorization': 'Bearer xoxb-426320088134-428368238130-mOzBKQmAasWgqE022eziMfcO',
+            },
+            body: JSON.stringify({text: 'Hello!', channel: payload.event.item.channel})
+        })
+        .then(response => {
+            console.log(response);
+        })
+
+    }
+
+    
 
 })
 
